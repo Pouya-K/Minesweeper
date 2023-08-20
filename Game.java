af1815d3dd1f15633045ec8d6c99f72bf4872224
Pouya-K <pouyakarimi86@gmail.com>
2023-08-20 14:20:47 -0400

    final working game

--- Game.java
+++ Game.java
@@ -5,23 +5,62 @@ import java.awt.event.MouseEvent;
 
 public class Game {
     JFrame frame = new JFrame("Minesweeper");
+    Drawing drawing = new Drawing();
+    private Color label1 = Color.black, label2 = Color.black, label3 = Color.black;
+    private Mousehandler mouseListen = new Mousehandler();
     public Game(){
-        frame.setSize(500,500);
+        frame.setSize(300,250);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new Drawing());
-        frame.addMouseListener(new Mousehandler());
+        frame.add(drawing);
+        frame.addMouseListener(mouseListen);
+        frame.addMouseMotionListener(mouseListen);
         frame.setVisible(true);
     }
     class Drawing extends JComponent{
         public void paint(Graphics g){
-            g.setColor(Color.black);
+            g.setColor(Color.red);
             g.setFont(new Font("Serif", Font.BOLD, 30));
-            g.drawString("Minesweeper", (getWidth() - g.getFontMetrics().stringWidth("Minesweeper")) / 2,50);
+            g.drawString("Minesweeper", (getWidth() - g.getFontMetrics().stringWidth("Minesweeper")) / 2,40);
+            g.setColor(label1);
+            g.drawString("Beginner", (getWidth() - g.getFontMetrics().stringWidth("Beginner")) / 2,90);
+            g.setColor(label2);
+            g.drawString("Intermediate", (getWidth() - g.getFontMetrics().stringWidth("Intermediate")) / 2,140);
+            g.setColor(label3);
+            g.drawString("Expert", (getWidth() - g.getFontMetrics().stringWidth("Expert")) / 2,190);
         }
     }
     class Mousehandler extends MouseAdapter{
         public void mouseClicked(MouseEvent e){
-
+            if(e.getX()>=95 && e.getX()<=200 && e.getY()>=95 && e.getY()<=120){
+                frame.dispose();
+                new Level('b');
+            }
+            else if(e.getX()>=70 && e.getX()<=230 && e.getY()>=150 && e.getY()<=165){
+                frame.dispose();
+                new Level('i');
+            }
+            else if(e.getX()>=105 && e.getX()<=190 && e.getY()>=200 && e.getY()<=215){
+                frame.dispose();
+                new Level('x');
+            }
+            drawing.repaint();
+        }
+        public void mouseMoved(MouseEvent e){
+            if(e.getX()>=95 && e.getX()<=200 && e.getY()>=95 && e.getY()<=120){
+                label1 = Color.red;
+            }
+            else if(e.getX()>=70 && e.getX()<=230 && e.getY()>=150 && e.getY()<=165){
+                label2 = Color.red;
+            }
+            else if(e.getX()>=105 && e.getX()<=190 && e.getY()>=200 && e.getY()<=215){
+                label3 = Color.red;
+            }
+            else{
+                label1 = Color.black;
+                label2 = Color.black;
+                label3 = Color.black;
+            }
+            drawing.repaint();
         }
     }
     public static void main(String[] args) {
--- Level.java
+++ Level.java
@@ -2,40 +2,47 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.util.Timer;
 import java.util.ArrayList;
+import java.util.TimerTask;
 
 public class Level {
     private JFrame frame = new JFrame("Minesweeper");
-    private Drawing drawing = new Drawing();
+    public static Drawing drawing;
     private boolean leftClick = false, rightClick = false;
     private int flagsLeft = 10;
     public static Square lostSquare;
+    private int time = 0;
+    private long timeTicker;
     public static ArrayList<Square> checkedSquares = new ArrayList<Square>();
     private int screenWidth, screenHeight, numOfMines, gridWidth, gridHeight;
     private Square[][] grid;
     public static boolean gameOver = false;
     private boolean gameWon = false;
     public Level(char level){
+        drawing = new Drawing();
+        timeTicker = System.currentTimeMillis();
         if(level == 'b'){
             numOfMines = 10;
             gridWidth = 9;
             gridHeight = 9;
             screenWidth = 310;
-            screenHeight = 430;
+            screenHeight = 480;
         }
         else if(level == 'i'){
             numOfMines = 40;
             gridWidth = 16;
             gridHeight = 16;
             screenWidth = 520;
-            screenHeight = 640;
+            screenHeight = 690;
         }
         else{
             numOfMines = 99;
             gridWidth = 30;
             gridHeight = 16;
             screenWidth = 940;
-            screenHeight = 640;
+            screenHeight = 690;
         }
         grid = new Square[gridHeight][gridWidth];
         generateSquares();
@@ -44,6 +51,9 @@ public class Level {
         frame.add(drawing);
         frame.addMouseListener(new Mousehandler());
         frame.setVisible(true);
+        int gap = 1000/60;
+        Timer timer = new Timer();
+        timer.schedule(new Runner(), 0,gap);
     }
     public void generateSquares(){
         int x = 20, y = 110;
@@ -71,22 +81,70 @@ public class Level {
             g.fillRect(0,0,getWidth(),getHeight());
             g.setColor(Color.darkGray);
             g.fillRect(10,10,getWidth()-20, 80);
-            g.fillRect(10,100, getWidth()-20, getHeight() - 110);
+            g.fillRect(10,100, getWidth()-20, getHeight() - 160);
             g.setColor(Color.black);
             g.fillRect(20,20, 100, 60);
             g.fillRect(getWidth()-120,20, 100, 60);
+            g.setColor(Color.lightGray);
+            g.fillRect(getWidth()/2-25, 25, 50, 50);
+            checkForWin();
+            String str = "";
             if(gameOver){
                 for (Square[] array : grid) {
                     for (Square gridSpot : array) {
                         if(gridSpot.isMine()) gridSpot.turnVisible();
                     }
                 }
+                str = "\uD83D\uDE35";
+            }
+            else if(gameWon){
+                for (Square[] array : grid) {
+                    for (Square gridSpot : array) {
+                        if(gridSpot.isMine()) gridSpot.turnFlag();
+                    }
+                }
+                str = "\uD83D\uDE0E";
+            }
+            else{
+                str = "\uD83D\uDE42";
+            }
+            g.setFont(new Font("Serif", Font.BOLD, 40));
+            g.drawString(str, getWidth()/2 - (g.getFontMetrics().stringWidth(str)) / 2, 65);
+            g.setColor(Color.lightGray);
+            g.fillRect(getWidth()/2-50, getHeight()-50, 45, 45);
+            g.fillRect(getWidth()/2, getHeight()-50, 45, 45);
+            str = "\uD83C\uDFE0";
+            g.setFont(new Font("Serif", Font.BOLD, 35));
+            g.drawString(str, getWidth()/2-27-(g.getFontMetrics().stringWidth(str)) / 2, getHeight()-15);
+            str = "\uD83D\uDD04";
+            g.drawString(str, getWidth()/2+22-(g.getFontMetrics().stringWidth(str)) / 2, getHeight()-15);
+
+            try {
+                File font_file = new File("digital-7.ttf");
+                Font font = Font.createFont(Font.TRUETYPE_FONT, font_file);
+                Font sizedFont = font.deriveFont(60f);
+                g.setFont(sizedFont);
+            } catch (Exception e) {
+            }
+            g.setColor(Color.red);
+            if(flagsLeft<0) str = "0"+flagsLeft;
+            else if(flagsLeft<10) str = "00" + flagsLeft;
+            else if(flagsLeft<100) str = "0" + flagsLeft;
+            g.drawString(str, 115-g.getFontMetrics().stringWidth(str), 70);
+            if(time<10) str = "00"+time;
+            else if(time<100) str = "0"+time;
+            else str = ""+time;
+            g.drawString(str, getWidth()-25-g.getFontMetrics().stringWidth(str), 70);
+            if(System.currentTimeMillis() - timeTicker >= 1000 && !gameWon && !gameOver){
+                time++;
+                timeTicker = System.currentTimeMillis();
             }
             drawGrid(g);
+
         }
         public void drawGrid(Graphics g){
             g.setColor(Color.lightGray);
-            g.fillRect(20,110, getWidth()-40, getHeight() - 130);
+            g.fillRect(20,110, getWidth()-40, getHeight() - 180);
             for (Square[] array : grid) {
                 for (Square gridSpot : array) {
                     if(!gridSpot.getVisibility()){
@@ -146,16 +204,49 @@ public class Level {
                 }
             }
         }
+        public void checkForWin(){
+            boolean won = false, notWon = false;
+            for (Square[] array : grid) {
+                for (Square gridSpot : array) {
+                    if(gridSpot.isMine() && gridSpot.isFlag()) won = true;
+                    else notWon = true;
+                }
+            }
+            if(won && !notWon) gameWon = true;
 
+            won = false;
+            notWon = false;
+            for (Square[] array : grid) {
+                for (Square gridSpot : array) {
+                    if(!gridSpot.isMine() && gridSpot.isVisible) won = true;
+                    else if(!gridSpot.isMine() && !gridSpot.isVisible) notWon = true;
+                }
+            }
+            if(won && !notWon) gameWon = true;
+        }
     }
     class Mousehandler extends MouseAdapter{
         public void mousePressed(MouseEvent e){
+            if(e.getX() >= drawing.getWidth()/2-50 && e.getX()<= drawing.getWidth()/2-5 && e.getY()>=drawing.getHeight()-20 && e.getY()<=drawing.getHeight()+25){
+                frame.dispose();
+                new Game();
+            }
+            else if(e.getX()>=drawing.getWidth()/2 && e.getX()<=drawing.getWidth()/2+45 && e.getY()>=drawing.getHeight()-20 && e.getY()<=drawing.getHeight()+25){
+                flagsLeft = 10;
+                time = 0;
+                timeTicker = System.currentTimeMillis();
+                gameWon = false;
+                gameOver = false;
+                lostSquare = null;
+                checkedSquares.clear();
+                generateSquares();
+            }
             if(e.getButton() == MouseEvent.BUTTON1) leftClick = true;
             else if(e.getButton() == MouseEvent.BUTTON3) rightClick = true;
             for(int i = 0; i<grid.length; i++){
                 for(int j = 0; j<grid[i].length; j++){
                     Square space = grid[i][j];
-                    if(e.getX() >= space.x && e.getX() <= space.x+30 && e.getY() - 30 >= space.y && e.getY() - 30 <= space.y+30 && !gameOver){
+                    if(e.getX() >= space.x && e.getX() <= space.x+30 && e.getY() - 30 >= space.y && e.getY() - 30 <= space.y+30 && !gameOver && !gameWon){
                         if((rightClick && leftClick) || e.getButton() == MouseEvent.BUTTON2){
                             if(!space.isMine() && space.isVisible){
                                 space.chord(grid, i, j);
@@ -178,16 +269,16 @@ public class Level {
                     }
                 }
             }
-            drawing.repaint();
         }
         public void mouseReleased(MouseEvent e){
             if(e.getButton() == MouseEvent.BUTTON1) leftClick = false;
             else if(e.getButton() == MouseEvent.BUTTON3) rightClick = false;
-            drawing.repaint();
         }
     }
-
-    public static void main(String[] args) {
-        new Level('b');
+}
+class Runner extends TimerTask {
+    public void run(){
+        Level.drawing.repaint();
     }
 }
+
